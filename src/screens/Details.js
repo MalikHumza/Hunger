1c470e9f84b95b470c6aa0760b878eac405b582e
MalikHumza <humzamalik1146@gmail.com>
2021-09-16 05:25:29 +0500

    FetchingResturantImages

--- src/screens/Details.js
+++ src/screens/Details.js
@@ -1,27 +1,45 @@
-import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, Text, View, FlatList, Image} from 'react-native';
 import yelp from '../api/yelp';
 
-const Details = ({route : {params}}) => {
-    const [result, setResult] = useState(null);
-    const {id} = params;
-    console.log(id);
-    console.log(result);
+const Details = ({route: {params}}) => {
+  const [result, setResult] = useState(null);
+  const {id} = params;
 
-    const getResult = async(id)=> {
-       const response = await yelp.get(`/${id}`);
-       setResult(response.data);
+  const getResult = async id => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
+  if (!result) {
+    return null;
+  }
+
+  return (
+    <View>
+      <FlatList
+        data={result.photos}
+        keyExtractor={humza => humza}
+        renderItem={({item}) => {
+          return <Image style={styles.image} source={{uri: item}}/>;
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    image: {
+        height: 200,
+        width: 200,
+        borderRadius: 5,
+        marginHorizontal: 100,
+        marginVertical: 20,
     }
-    useEffect(() => {
-        getResult(id);
-    }, [])
-    return (
-        <View>
-            <Text>Deatils Page</Text>
-        </View>
-    )
-}
+});
 
-const styles = StyleSheet.create({})
-
-export default Details
+export default Details;
